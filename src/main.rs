33eed5f6f9af22af374489da818976bf8a417a3e
WillMansell <Wombat2000@yahoo.com>
2022-05-08 12:08:07 +0100

    Multiple visitors

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,50 @@
 
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new( name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor (&self) {
+        println!("{}", self.greeting);
+    }
+
+
+}
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read string");
     your_name
+      .trim()
+      .to_lowercase()
 }
 
 fn main() {
-    let visitor_list = ["bert", "steve", "fred"];
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve","Hi Steve. Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
     println!("Hello, what`s your name?");
-    let mut allow_them_in = false;
     let name = what_is_your_name();
-    for visitor in &visitor_list {
-        if visitor == &name {
-            allow_them_in = true;
+    let known_visitor = visitor_list
+        .iter()
+        .find(|visitor| visitor.name == name);
+    
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => println!("You are not on the visitor list. Please leave.")
         }
-    }
-
-    if allow_them_in {
-        println!("Come in");
-    } else {
-        println!("Stay away");
-    }
 }
